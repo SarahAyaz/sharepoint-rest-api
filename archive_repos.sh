d888237788cc8aeb340f51793a95bbd00d0cac21
Sarah Ayaz <sarah.ayaz12@gmail.com>
2023-07-24 16:36:55 +0200

    Add yaml config file, script update to clone files from yaml

--- archive_repos.sh
+++ archive_repos.sh
@@ -1,18 +1,23 @@
-repos=("airflow")
 project="Business-Intelligence"
+repo_list=()
+while IFS= read -r line
+do
+    file_name="${line#- }"
+    repo_list+=("$file_name")
+done < <(yq e '.archive_file_config.list' ./parameters.yaml)
 
-for repo in ${repos[@]}
+for repo in ${repo_list[@]}
 do
     echo "Cloning $repo"
-    repo_id=$(az repos show -r $repo --query id -p $project | tr -d '"')
     az repos show -r $repo --query sshUrl -p $project | tr -d '"' | while read sshUrl ; do git clone ${sshUrl} archive/$repo ; done
     cd archive/$repo
     git archive --format zip -o ../$repo.zip HEAD
     cd ..
-    rm -r $repo
+    rm -r -f $repo
     cd ..
+    repo_id=$(az repos show -r $repo --query id -p $project | tr -d '"')
     echo "Deleting $repo with id $repo_id"
-    az repos delete --id $repo_id -p $project
+    # az repos delete --id $repo_id -p $project
 done    
 
 echo "Archive complete"
